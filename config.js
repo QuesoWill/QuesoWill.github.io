240ae125809e539be6637490e2a92487c7a5216d
QuesoWill <will.scharpf@richmond.edu>
2026-07-24 14:13:03 -0400

    Fix shortBio formatting in config.js

--- config.js
+++ config.js
@@ -38,7 +38,7 @@ const CONFIG = {
   // 1-2 sentences. Keep it punchy.
   // =========================================================
 
-  shortBio: `Hello, my name is Will and I am a 2026 graduate of the University of Richmond with B.A. in Leadership Studies with a Minor in Data Science and Statistics. With a passion of creating data visualizations,`
+  shortBio: `Hello, my name is Will and I am a 2026 graduate of the University of Richmond with B.A. in Leadership Studies with a Minor in Data Science and Statistics. With a passion of creating data visualizations`,
 
 
   // =========================================================
